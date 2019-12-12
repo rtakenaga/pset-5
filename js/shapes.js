c6cd4cea20f26737efdb03d165be5c84297cca94
KING RIO BIRD <ryomatakenaga@gmail.com>
2019-12-11 22:56:22 -0500

    finished everything

--- js/shapes.js
+++ js/shapes.js
@@ -20,7 +20,12 @@ window.onload = function() {
 
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
-}
+    document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
+    document.getElementById("triangle").onclick = drawTriangle;
+    document.getElementById("smile").onclick = drawFace;
+    document.getElementById("pyramid").onclick = drawPyramid;
+
+  }
 
 /*
  * Exercise 1.
@@ -32,6 +37,7 @@ window.onload = function() {
      alert("Your message is too long. Please keep your message under 50 characters.");
      message = prompt("Message:")
    }
+
      let canvas = document.getElementById('student-canvas-1');
      let ctx = canvas.getContext('2d');
      ctx.font = '48px sans-serif';
@@ -89,30 +95,178 @@ const drawRectangle = function() {
  * Exercise 3.
  */
 
-const drawColoredRectangle = function() {
-    // write your exercise 3 code here
-};
+ const drawColoredRectangle = function() {
+   const canvas = document.getElementById("student-canvas-3");
+   const ctx = canvas.getContext("2d");
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   do {
+     var color = (prompt("Color: "))
+     if (color == null) {
+       break;
+     }
+     color = String(color)
+     var color_case = color.toLowerCase()
+     if (color_case != "green" && color_case != "black" && color_case != "blue" && color_case != "orange" && color_case != "purple" && color_case != "red" && color_case != "yellow") {
+       alert(color + " is not a supported color.")
+     }
+   } while (color_case != "green" && color_case != "black" && color_case != "blue" && color_case != "orange" && color_case != "purple" && color_case != "red" && color_case != "yellow")
+
+   if (color != null) {
+     ctx.fillStyle = color_case;
+     ctx.fillRect(10, 10, 100, 50);
+   }
+
+ };
 
 /*
  * Exercise 4.
  */
 
-const drawTriangle = function() {
-    // write your exercise 4 code here
-};
+ const drawTriangle = function() {
+   const canvas = document.getElementById("student-canvas-4");
+   const ctx = canvas.getContext("2d");
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+   do {
+     var side1 = prompt("Side 1: ")
+     if (side1 == null) {
+       break;
+     }
+     var side2 = prompt("Side 2: ")
+     if (side2 == null) {
+       break;
+     }
+     var side3 = prompt("Side 3: ")
+     if (side3 == null) {
+       break;
+     }
+
+     var hypotenuse = Math.max(side1, side2, side3)
+     var height = Math.min (side1, side2, side3)
+     var base = Math.sqrt(hypotenuse*hypotenuse - height*height)
+
+     side1 = Number(side1)
+     side2 = Number(side2)
+     side3 = Number(side3)
+
+     if (base*base + height*height != hypotenuse*hypotenuse || base == 0 || height == 0 || hypotenuse == 0  || side1+side2+side3-hypotenuse-height != base) {
+       alert("That's not a valid right triangle.")
+   }
+     else if (isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+       alert("One of your sides is not a number.")
+   }
+     else if (base > 1024 || height > 512 || hypotenuse > 1310720) {
+       alert("Your triangle won't fit on the canvas.")
+   }
+ }   while ((Math.floor(base)*Math.floor(base) + height*height != hypotenuse*hypotenuse) || isNaN(side1) || isNaN(side2) || isNaN(side3) || base > 1024 || height > 512 || hypotenuse > 1310720 || base == 0 || height == 0 || hypotenuse == 0)
+
+   if ((base*base + height*height == hypotenuse*hypotenuse) && (base < 1024 && height < 512 && hypotenuse < 1145) && (base != 0 && height != 0 && hypotenuse != 0) && (base != null && height != null && hypotenuse != null)) {
+     height = height + 25
+     base = base + 25
+     ctx.beginPath();
+     ctx.moveTo(25, 25);
+     ctx.lineTo(25, height);
+     ctx.lineTo(base, height)
+     ctx.lineTo(25, 25)
+     ctx.stroke();
+ }
+ };
+
 
 /*
  * Exercise 5.
  */
 
-const drawFace = function() {
-    // write your exercise 4 code here
-};
+ const drawFace = function() {
+   const canvas = document.getElementById("student-canvas-5");
+   const ctx = canvas.getContext("2d");
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+   do {
+   var radius = (prompt("Radius: "))
+   if (radius == null) {
+     break;
+   }
+   if (radius < 32) {
+     alert("Your radius must be at least 32.")
+   }
+   else if (isNaN(radius)) {
+     alert("Your radius is not a number.")
+   }
+   else if (radius > 256) {
+     alert("Your smiley face won't fit on the canvas.")
+   }
+ } while (radius > 256 || isNaN(radius) || radius < 32)
+
+ var eyesRadius = 0.15 * radius
+ var mouthRadius = 0.7 * radius
+
+
+ ctx.beginPath();
+ ctx.arc(512, 256, radius, 0, 2 * Math.PI);
+ ctx.stroke();
+ ctx.closePath();
+ ctx.beginPath();
+ ctx.arc(512, 256, mouthRadius, 0, Math.PI);
+ ctx.stroke();
+ ctx.closePath();
+ ctx.beginPath();
+ ctx.arc(512 - 0.4 * radius, 256 - 0.4 * radius, eyesRadius, 0, 2 * Math.PI);
+ ctx.stroke();
+ ctx.closePath();
+ ctx.beginPath();
+ ctx.arc(512 + 0.4 * radius, 256 - 0.4* radius, eyesRadius, 0, 2 * Math.PI);
+ ctx.stroke();
+ ctx.closePath();
+
+ };
 
 /*
  * Exercise 6 (extra credit).
  */
 
-const drawPyramid = function() {
-    // write your exercise 5 code here
-};
+ const drawPyramid = function() {
+ const canvas = document.getElementById("student-canvas-6");
+   const ctx = canvas.getContext("2d");
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+   var distance = 0;
+   var height = 0;
+   var adjustDistance = 0;
+   var adjustHeight = 0;
+   var check = 5;
+   var side;
+
+   do {
+       var side = prompt("Side: ")
+       if (side == null) {
+         break;
+       }
+       if (side < 1) {
+        alert("Your block size must be at least 1.")
+       }
+       else if (side > 100) {
+        alert("Your pyramid will fit on the canvas")
+       }
+       else if (isNaN(side)) {
+        alert("Your block size is not a number.")
+       }
+     } while (isNaN(side) || side > 100 || side < 1)
+     distance = Number(distance);
+     height = Number(height);
+     side = Number(side);
+     for (x = 5; x > 0; x--) {
+     check = x
+     while(check >= 1) {
+      ctx.beginPath();
+      ctx.rect(10 + distance, (502 - side) - height, side,  side);
+      ctx.stroke();
+      ctx.closePath();
+      distance = distance + side
+      check--
+     }
+      adjustingDistance++
+      distance = adjustingDistance * (1/2 * side)
+      adjustingHeight++
+      height = adjustingHeight * side
+     }
+ };
